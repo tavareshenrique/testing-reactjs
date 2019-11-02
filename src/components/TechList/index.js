7a4d864821511ee0bde3f63040daa00a919cbb86
Henrique Tavares <ihenrits@gmail.com>
2019-11-02 18:24:58 -0300

    Testando formulario

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
 
   function handleAddTech() {
-    setTechs([...techs, 'Node.js']);
+    setTechs([...techs, newTech]);
+    setNewTech('');
   }
 
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech} >
       <ul data-testid="tech-list">
         {techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
-      <button onClick={handleAddTech} >Adicionar</button>
-    </div>
+
+      <label htmlFor="tech">Tech</label>
+      <input type="text" id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
+      <button type="submit" >Adicionar</button>
+    </form>
   );
 }
