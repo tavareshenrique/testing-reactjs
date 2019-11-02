dadf2f9ca250bb0afb750636128f0e2bfca03d52
Henrique Tavares <ihenrits@gmail.com>
2019-11-02 19:39:37 -0300

    Mock do useDispatch

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { addTech } from '~/store/modules/techs/actions';
 
 export default function TechList() {
   const [newTech, setNewTech] = useState('');
@@ -9,7 +10,7 @@ export default function TechList() {
   const techs = useSelector(state => state.techs);
 
   function handleAddTech() {
-    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } });
+    dispatch(addTech(newTech));
     setNewTech('');
   }
 
